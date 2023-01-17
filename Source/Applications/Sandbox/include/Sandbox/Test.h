28229db07527293911ec4a16951ab925aea42233
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-01-17 18:08:12 +0530

    Platform Test

--- Source/Applications/Sandbox/include/Sandbox/Test.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-void Test()
-{
-	std::cout << "Welcome to Nexus" << std::endl;
-}
--- Source/Applications/Sandbox/src/Main.cpp
+++ Source/Applications/Sandbox/src/Main.cpp
@@ -1,6 +1,17 @@
-#include "Sandbox/Test.h"
+#include "Platform/Manager.h"
 
 int main()
 {
-	Test();
+	Nexus::Platform::Initialize();
+
+	Nexus::Platform::Window appWindow = { 800,600,nullptr };
+	Nexus::Platform::Manager::CreateWindow(&appWindow, "Sandbox");
+
+	while (Nexus::Platform::Manager::IsOpen(&appWindow))
+	{
+		Nexus::Platform::Manager::PollEvents();
+	}
+
+	Nexus::Platform::Manager::DestroyWindow(&appWindow);
+	Nexus::Platform::Shutdown();
 }
