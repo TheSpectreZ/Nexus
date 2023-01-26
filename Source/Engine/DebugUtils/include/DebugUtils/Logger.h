434e184ca857e976ba56973719a0c9f08b83f84f
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-01-26 17:10:16 +0530

    [Logger] Info and Debug

--- Source/Engine/DebugUtils/include/DebugUtils/Logger.h
+++ Source/Engine/DebugUtils/include/DebugUtils/Logger.h
@@ -22,7 +22,9 @@ namespace Nexus
 #define NEXUS_LOG_INIT ::Nexus::DebugUtils::Logger::Init();
 #define NEXUS_LOG_SHUT ::Nexus::DebugUtils::Logger::Shut();
 
+#define NEXUS_LOG_DEBUG(...) ::Nexus::DebugUtils::Logger::_logger->debug(__VA_ARGS__);
 #define NEXUS_LOG_TRACE(...) ::Nexus::DebugUtils::Logger::_logger->trace(__VA_ARGS__);
+#define NEXUS_LOG_INFO(...) ::Nexus::DebugUtils::Logger::_logger->info(__VA_ARGS__);
 #define NEXUS_LOG_WARN(...) ::Nexus::DebugUtils::Logger::_logger->warn(__VA_ARGS__);
 #define NEXUS_LOG_ERROR(...) ::Nexus::DebugUtils::Logger::_logger->error(__VA_ARGS__);
 
@@ -32,7 +34,9 @@ namespace Nexus
 #define NEXUS_LOG_SHUT
 
 
+#define NEXUS_LOG_DEBUG(...)
 #define NEXUS_LOG_TRACE(...)
+#define NEXUS_LOG_INFO(...)
 #define NEXUS_LOG_WARN(...)
 #define NEXUS_LOG_ERROR(...)
 
