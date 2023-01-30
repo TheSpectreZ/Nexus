ed221150cc15a7a51a361827738d0d8a1a8007ee
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-01-30 19:34:48 +0530

    Buffer Allocation with VMA

--- Source/Engine/Graphics/include/Graphics/Resource.h
+++ Source/Engine/Graphics/include/Graphics/Resource.h
@@ -2,40 +2,53 @@
 #include "Build.h"
 #include "vulkan/vulkan.h"
 
+VK_DEFINE_HANDLE(VmaAllocation)
+
 namespace Nexus
 {
 	namespace Graphics
 	{
-		class NEXUS_GRAPHICS_API Buffer
+		class NEXUS_GRAPHICS_API VertexBuffer
 		{
 		public:
-			void Create(VkBufferUsageFlagBits usage, VkDeviceSize size);
+			void Create(uint32_t count,VkDeviceSize stride,void* data);
 			void Destroy();
-
-			static void CopyBuffer(VkCommandBuffer buffer,Buffer& src, Buffer& dst, VkDeviceSize size);
-
-			VkBuffer& Get() { return m_handle; }
-			VkBuffer Get() const { return m_handle; }
-
-			VkDeviceSize GetSize() { return m_size; }
+			void Bind(VkCommandBuffer cmd);
 		private:
-			VkDeviceSize m_size;
-			VkBuffer m_handle;
+			VkBuffer m_buffer;
+			VmaAllocation m_allocation;
 		};
 
-		class NEXUS_GRAPHICS_API Memory
+		class NEXUS_GRAPHICS_API IndexBuffer
 		{
 		public:
-			void Allocate(Buffer& buffer, VkMemoryPropertyFlags flags);
-			void Free();
+			void Create(uint32_t count,VkDeviceSize stride,void* data);
+			void Destroy();
+			void Bind(VkCommandBuffer cmd);
 
-			void Map();
-			void Update(void* data);
-			void UnMap();
+			uint32_t GetIndexCount() { return m_indices; }
 		private:
-			VkDeviceMemory m_handle;
-			VkDeviceSize m_size;
-			void* m_data;
+			uint32_t m_indices;
+			VkBuffer m_buffer;
+			VmaAllocation m_allocation;
 		};
+
+		class NEXUS_GRAPHICS_API UniformBuffer
+		{
+		public:
+			void Create(VkDeviceSize size);
+			void Destroy();
+			void Update(void* data);
+
+			VkBuffer& Get() { return m_buffer; }
+			VkBuffer Get() const { return m_buffer; }
+		private:
+			void* m_Data;
+			VkDeviceSize m_size;
+			VkBuffer m_buffer;
+			VmaAllocation m_allocation;
+			uint32_t m_Info;
+		};
+
 	}
 }
--- Source/Engine/Graphics/src/Resource.cpp
+++ Source/Engine/Graphics/src/Resource.cpp
@@ -1,78 +1,159 @@
-#include "Graphics/Resource.h"
 #include "Backend.h"
+#include "Graphics/Resource.h"
 #include "vkAssert.h"
+#include <unordered_map>
 
-void Nexus::Graphics::Buffer::Create(VkBufferUsageFlagBits usage, VkDeviceSize size)
+void Copy(VkCommandBuffer cmd, VkBuffer src, VkBuffer dst, VkDeviceSize size)
+{
+	VkBufferCopy copy{};
+	copy.size = size;
+	copy.dstOffset = 0;
+	copy.srcOffset = 0;
+
+	vkCmdCopyBuffer(cmd, src, dst, 1, &copy);
+	NEXUS_LOG_DEBUG("Buffer Copied: size-{0}", size);
+}
+
+void Nexus::Graphics::VertexBuffer::Create(uint32_t count, VkDeviceSize stride,void* data)
+{
+	// Staging
+	VkBuffer stagingBuffer;
+	VmaAllocation stagingAllocation;
+	{
+		VkBufferCreateInfo Info{};
+		Info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+		Info.pNext = nullptr;
+		Info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		Info.size = stride * count;
+		Info.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+
+		VmaAllocationCreateInfo info{};
+		info.usage = VMA_MEMORY_USAGE_AUTO;
+		info.flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT;
+
+		VmaAllocationInfo ainfo{};
+		vmaCreateBuffer(Backend::GetAllocator(), &Info, &info, &stagingBuffer, &stagingAllocation, &ainfo);
+
+		memcpy(ainfo.pMappedData, data, stride * count);
+	}
+
+	VkBufferCreateInfo Info{};
+	Info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	Info.pNext = nullptr;
+	Info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	Info.size = stride * count;
+	Info.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+
+	VmaAllocationCreateInfo info{};
+	info.usage = VMA_MEMORY_USAGE_AUTO;
+
+	_VKR = vmaCreateBuffer(Backend::GetAllocator(), &Info, &info, &m_buffer, &m_allocation, nullptr);
+	CHECK_HANDLE(m_buffer, VkBuffer)
+	NEXUS_LOG_TRACE("Vertex Buffer Created: {0}", count * stride)
+
+	auto cmdbuffer = Backend::BeginSingleTimeCommands();
+	Copy(cmdbuffer, stagingBuffer, m_buffer, stride * count);
+	Backend::EndSingleTimeCommands(cmdbuffer);
+
+	vmaDestroyBuffer(Backend::GetAllocator(), stagingBuffer, stagingAllocation);
+}
+
+void Nexus::Graphics::VertexBuffer::Destroy()
+{
+	vmaDestroyBuffer(Backend::GetAllocator(), m_buffer, m_allocation);
+}
+
+void Nexus::Graphics::VertexBuffer::Bind(VkCommandBuffer buffer)
+{
+	VkDeviceSize off[] = { 0 };
+	vkCmdBindVertexBuffers(buffer, 0, 1, &m_buffer, off);
+}
+
+void Nexus::Graphics::IndexBuffer::Create(uint32_t count,VkDeviceSize stride,void* data)
+{
+	m_indices = count;
+
+	// Staging
+	VkBuffer stagingBuffer;
+	VmaAllocation stagingAllocation;
+	{
+		VkBufferCreateInfo Info{};
+		Info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+		Info.pNext = nullptr;
+		Info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		Info.size = stride * count;
+		Info.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+
+		VmaAllocationCreateInfo info{};
+		info.usage = VMA_MEMORY_USAGE_AUTO;
+		info.flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT;
+
+		VmaAllocationInfo ainfo{};
+		vmaCreateBuffer(Backend::GetAllocator(), &Info, &info, &stagingBuffer, &stagingAllocation, &ainfo);
+
+		memcpy(ainfo.pMappedData, data, stride * count);
+	}
+
+	VkBufferCreateInfo Info{};
+	Info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	Info.pNext = nullptr;
+	Info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	Info.size = stride * count;
+	Info.usage = VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+
+	VmaAllocationCreateInfo info{};
+	info.usage = VMA_MEMORY_USAGE_AUTO;
+
+	_VKR = vmaCreateBuffer(Backend::GetAllocator(), &Info, &info, &m_buffer, &m_allocation, nullptr);
+	CHECK_HANDLE(m_buffer, VkBuffer)
+	NEXUS_LOG_TRACE("Index Buffer Created: {0} , {1}", m_indices, m_indices * stride)
+
+	auto cmdbuffer = Backend::BeginSingleTimeCommands();
+	Copy(cmdbuffer, stagingBuffer, m_buffer, stride * count);
+	Backend::EndSingleTimeCommands(cmdbuffer);
+
+	vmaDestroyBuffer(Backend::GetAllocator(), stagingBuffer, stagingAllocation);
+}
+
+void Nexus::Graphics::IndexBuffer::Destroy()
+{
+	vmaDestroyBuffer(Backend::GetAllocator(), m_buffer, m_allocation);
+}
+
+void Nexus::Graphics::IndexBuffer::Bind(VkCommandBuffer buffer)
+{
+	vkCmdBindIndexBuffer(buffer, m_buffer, 0, VK_INDEX_TYPE_UINT32);
+}
+
+void Nexus::Graphics::UniformBuffer::Create(VkDeviceSize size)
 {
 	m_size = size;
 
 	VkBufferCreateInfo Info{};
 	Info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
 	Info.pNext = nullptr;
-	Info.usage = usage;
-	Info.size = size;
-	Info.flags = 0;
 	Info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	Info.size = size;
+	Info.usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
 
-	_VKR = vkCreateBuffer(Backend::GetDevice(), &Info, nullptr, &m_handle);
-	CHECK_HANDLE(m_handle, VkBuffer)
-	NEXUS_LOG_TRACE("Vulkan Buffer Created")
+	VmaAllocationCreateInfo info{};
+	info.usage = VMA_MEMORY_USAGE_AUTO;
+	info.flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT;
+
+	_VKR = vmaCreateBuffer(Backend::GetAllocator(), &Info, &info, &m_buffer,&m_allocation, nullptr);
+	CHECK_HANDLE(m_buffer,VkBuffer)
+	NEXUS_LOG_TRACE("Uniform Buffer Created: {0}",size);
+
+	vmaMapMemory(Backend::GetAllocator(), m_allocation, &m_Data);
 }
 
-void Nexus::Graphics::Buffer::Destroy()
+void Nexus::Graphics::UniformBuffer::Destroy()
 {
-	vkDestroyBuffer(Backend::GetDevice(), m_handle, nullptr);
+	vmaUnmapMemory(Backend::GetAllocator(), m_allocation);
+	vmaDestroyBuffer(Backend::GetAllocator(), m_buffer, m_allocation);
 }
 
-void Nexus::Graphics::Buffer::CopyBuffer(VkCommandBuffer buffer,Buffer& src, Buffer& dst, VkDeviceSize size)
+void Nexus::Graphics::UniformBuffer::Update(void* data)
 {
-	VkBufferCopy copy{};
-
-	copy.dstOffset = 0;
-	copy.srcOffset = 0;
-	copy.size = size;
-
-	vkCmdCopyBuffer(buffer, src.Get(), dst.Get(), 1, &copy);
-}
-
-void Nexus::Graphics::Memory::Allocate(Buffer& buffer, VkMemoryPropertyFlags flags)
-{
-	m_size = buffer.GetSize();
-
-	VkMemoryRequirements req{};
-	vkGetBufferMemoryRequirements(Backend::GetDevice(), buffer.Get(), &req);
-
-	VkMemoryAllocateInfo Info{};
-	Info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-	Info.pNext = nullptr;
-	Info.allocationSize = req.size;
-	Info.memoryTypeIndex = FindMemoryType(Backend::GetPhysicalDevice(), req.memoryTypeBits, flags);
-
-	_VKR = vkAllocateMemory(Backend::GetDevice(), &Info, nullptr, &m_handle);
-	CHECK_HANDLE(m_handle,VkDeviceMemory);
-
-	_VKR = vkBindBufferMemory(Backend::GetDevice(), buffer.Get(), m_handle, 0);
-	CHECK_LOG_VKR
-
-	NEXUS_LOG_TRACE("Vulkan Memory Allocated: {0}",req.size)
-}
-
-void Nexus::Graphics::Memory::Free()
-{
-	vkFreeMemory(Backend::GetDevice(), m_handle, nullptr);
-}
-
-void Nexus::Graphics::Memory::Map()
-{
-	vkMapMemory(Backend::GetDevice(), m_handle, 0, m_size, 0, &m_data);
-}
-
-void Nexus::Graphics::Memory::Update(void* data)
-{
-	memcpy(m_data, data, m_size);
-}
-
-void Nexus::Graphics::Memory::UnMap()
-{
-	vkUnmapMemory(Backend::GetDevice(), m_handle);
+	memcpy(m_Data, data, m_size);
 }
