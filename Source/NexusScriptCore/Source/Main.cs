da3f43e743fe0298c4c6d87a85212ef54a806cd5
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-04-23 20:34:52 +0530

    [ScriptCore] TestClass

---
+++ Source/NexusScriptCore/Source/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Nexus
+{
+    public class Main
+    {
+        public float FloatVar { get; set; }
+
+        public Main()
+        {
+            Console.WriteLine("Main Construtor");
+        }
+
+        ~Main()
+        {
+            Console.WriteLine("Main Destructor");
+        }
+
+        public void PrintMessage()
+        {
+            Console.WriteLine("Hello World From C#");
+        }
+
+        public void PrintInt(int value)
+        {
+            Console.WriteLine($"C# says: {value}");
+        }
+        
+        public void PrintCustomMessage(string message)
+        {
+            Console.WriteLine($"C# says: {message}");
+        }
+    }
+}
