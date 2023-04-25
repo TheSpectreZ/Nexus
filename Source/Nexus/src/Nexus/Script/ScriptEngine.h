2d75bda8711cc0f2e4901c31a1014911bcfd811c
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-04-26 01:07:09 +0530

    [Script Classes and Instances]

--- Source/Nexus/src/Nexus/Script/ScriptEngine.h
+++ Source/Nexus/src/Nexus/Script/ScriptEngine.h
@@ -1,14 +1,56 @@
 #pragma once
 
+extern "C" {
+	typedef struct _MonoDomain MonoDomain;
+	typedef struct _MonoAssembly MonoAssembly;
+	typedef struct _MonoClass MonoClass;
+	typedef struct _MonoObject MonoObject;
+	typedef struct _MonoImage MonoImage;
+	typedef struct _MonoMethod MonoMethod;
+}
+
 namespace Nexus
 {
 	class ScriptEngine
 	{
+		friend class ScriptInstance;
+		static ScriptEngine* s_Instance;
 	public:
 		static void Init();
 		static void Shut();
 	private:
-		static void InitMono();
-		static void ShutdownMono();
+		void InitMono();
+		void ShutdownMono();
+		
+		char* ReadBytes(const std::string& filepath, uint32_t* outSize);
+		void PrintAssemblyTypes(MonoAssembly* assembly,const char* name);
+		MonoAssembly* LoadAssembly(const std::string& assemblyPath);
+
+		void LoadCoreAssemblyClasses();
+	private:
+		MonoDomain* m_RootDomain;
+		MonoDomain* m_AppDomain;
+		MonoAssembly* m_CoreAssembly;
+		MonoImage* m_CoreAssemblyImage;
+
+		std::unordered_map<std::string, MonoClass*> m_EntityClasses;
+	};
+
+	class ScriptInstance
+	{
+	public:
+		ScriptInstance() = default;
+		ScriptInstance(const std::string& ClassName);
+
+		void InVokeOnCreate();
+		void InVokeOnUpdate(float ts);
+		void InVokeOnDestroy();
+	private:
+		MonoClass* m_Class;
+		MonoObject* m_Object;
+
+		MonoMethod* m_OnCreate;
+		MonoMethod* m_OnUpdate;
+		MonoMethod* m_OnDestroy;
 	};
 }
