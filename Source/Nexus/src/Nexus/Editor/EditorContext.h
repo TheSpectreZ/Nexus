54a2d26a5625acdfb02bf8f98d50b38a5d854cf3
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-04-25 05:51:12 +0530

    [Editor] Moved Vulkan Code for EditorContext to It's Platform Class

--- Source/Nexus/src/Nexus/Editor/EditorContext.h
+++ Source/Nexus/src/Nexus/Editor/EditorContext.h
@@ -1,21 +1,23 @@
 #pragma once
 #include "imgui.h"
 
-/// 
-/// Currently Supports Vulkan Only - Will Make it More Abstract When Other APIs are Added
-/// 
-
 namespace Nexus
 {
-
 	class EditorContext
 	{
 	public:
+		EditorContext() = default;
+		virtual ~EditorContext() = default;
+
 		static void Initialize();
 		static void Shutdown();
 
 		static void StartFrame();
 		static void Render();
+	private:
+		virtual void Start() = 0;
+		virtual void End() = 0;
+		
+		static EditorContext* s_Instance;
 	};
-
 }
