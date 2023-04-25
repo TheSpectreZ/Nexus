d76fb59925b83b244f95886fbe644c8992db2eed
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-04-26 01:07:37 +0530

    [ScriptCore] Entity and Player Test Script

---
+++ Source/NexusScriptCore/Source/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Nexus;
+
+namespace Sandbox
+{
+    public class Player : Entity
+    {
+        public override void OnCreate() 
+        {
+            Console.WriteLine("Player.OnCreate");
+        }
+        public override void OnUpdate(float ts)
+        {
+            Console.WriteLine($"Player.OnUpdate: {ts}");
+        }
+        public override void OnDestroy()
+        {
+            Console.WriteLine("Player.OnDestroy");
+        }
+    }
+}
