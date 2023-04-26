7a43d2b2fc34a7dca8f6fd214d09c2a76b40f332
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-04-27 04:45:56 +0530

    [Scene] Callbacks on Destruction

--- Source/Nexus/src/Nexus/Scene/Scene.h
+++ Source/Nexus/src/Nexus/Scene/Scene.h
@@ -9,17 +9,25 @@ namespace Nexus
 	
 	class Scene
 	{
-		friend class Entity;
-		friend class SceneBuildData;
-		friend class SceneRenderer;
 	public:
 		static Ref<Scene> Create();
 		void Clear();
 
 		Entity CreateEntity();
 		Entity CreateEntity(const std::string& name);
+		Entity CreateEntity(const std::string& name, UUID uuid);
 		void DestroyEntity(Entity entity);
+
 	private:
 		entt::registry m_registry;
+
+		std::function<void(Entity e)> EntityDestructionCallback;
+		std::function<void()> SceneDestructionCallback;
+
+		friend class Entity;
+		friend class SceneBuildData;
+		friend class SceneRenderer;
+		friend class SceneSerializer;
+		friend class SceneHeirarchy;
 	};
 }
