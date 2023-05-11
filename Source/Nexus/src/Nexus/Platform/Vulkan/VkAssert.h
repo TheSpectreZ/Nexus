6a5ea3fb22c5a588fafd2ebb6b35594c0589dae2
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-05-11 13:14:18 +0530

    [Vulkan] Minor Change in VkAssert

--- Source/Nexus/src/Nexus/Platform/Vulkan/VkAssert.h
+++ Source/Nexus/src/Nexus/Platform/Vulkan/VkAssert.h
@@ -10,9 +10,9 @@ static VkResult _VKR;
 #define CLEAR_VKR _VKR = VK_SUCCESS
 #define CHECK_LOG_VKR if(_VKR != VK_SUCCESS) { NEXUS_LOG_ERROR("VkResult: {0}",string_VkResult(_VKR)); CLEAR_VKR; }
 
-#define CHECK_HANDLE(handle,type) if(handle == nullptr)\
+#define CHECK_HANDLE(handle,type) CHECK_LOG_VKR \
+if(handle == nullptr)\
 {\
 NEXUS_LOG_ERROR("{0}:{1} is Null",#type,#handle) \
-CHECK_LOG_VKR \
 NEXUS_ASSERT(1,"Vulkan Handle Error");\
 }
