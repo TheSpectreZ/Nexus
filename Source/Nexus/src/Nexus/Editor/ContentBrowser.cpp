808bbaa3e7d7b89e8acb412102f1fc34377c2948
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-05-12 21:54:56 +0530

    [Editor] Content Browser Impl with Tables

--- Source/Nexus/src/Nexus/Editor/ContentBrowser.cpp
+++ Source/Nexus/src/Nexus/Editor/ContentBrowser.cpp
@@ -3,9 +3,63 @@
 
 #include "imgui.h"
 
+void Nexus::ContentBrowser::DrawDirectoryNodes(std::filesystem::path path)
+{
+	for (auto& dir : std::filesystem::directory_iterator(path))
+	{
+		if (!dir.is_directory())
+			continue;
+
+		const auto& p = dir.path();
+		auto filenameString = p.filename().string();
+
+		if (ImGui::TreeNode(filenameString.c_str()))
+		{
+			m_CurrentDirectory = p;
+			DrawDirectoryNodes(p);
+			ImGui::TreePop();
+		}
+	}
+}
+
+void Nexus::ContentBrowser::DrawDirectoryFiles(std::filesystem::path path)
+{
+	static float padding = 24.f;
+	static float thumbnailSize = 86.f;
+	static float cellsize = thumbnailSize + padding;
+
+	float panelWidth = ImGui::GetContentRegionAvail().x;
+
+	int columnCount = (int)(panelWidth / (cellsize));
+	columnCount = std::max(1, columnCount);
+
+	ImGui::Columns(columnCount, 0, false);
+
+	int i = 0;
+	for (auto& dir : std::filesystem::directory_iterator(path))
+	{
+		ImGui::PushID(i++);
+
+		const auto& path = dir.path();
+		auto filenameString = path.filename().string();
+
+		if (ImGui::Button(filenameString.c_str(), ImVec2(100.f, 100.f)))
+		{
+			if (dir.is_directory())
+				m_CurrentDirectory = path;	
+		}
+
+		ImGui::PopID();
+		ImGui::NextColumn();
+	}
+	ImGui::Columns(1);
+}
+
 void Nexus::ContentBrowser::SetContext(const std::string& projectRootPath)
 {
-	m_AssetDirectory = std::string(projectRootPath + "\\Assets");
+	m_AssetDirectory = std::string(projectRootPath + "\\Assets"); 
+	//[Note] Breaks when AssetDirectory is Root Directory for any Disk
+
 	m_CurrentDirectory = m_AssetDirectory;
 
 	if (!std::filesystem::is_directory(m_AssetDirectory))
@@ -16,65 +70,31 @@ void Nexus::ContentBrowser::Render()
 {
 	ImGui::Begin("Content Browser");
 
-	if (m_CurrentDirectory != std::filesystem::path(m_AssetDirectory))
+	if (ImGui::BeginTable("Content", 2, ImGuiTableFlags_BordersInnerV | ImGuiTableFlags_SizingFixedFit | ImGuiTableFlags_Resizable))
 	{
-		if (ImGui::Button("<-"))
+		ImGui::TableSetupColumn("Outline", 0, 250.f);
+		ImGui::TableSetupColumn("Files", ImGuiTableColumnFlags_WidthStretch);
+
+		ImGui::TableNextRow();
+		ImGui::TableSetColumnIndex(0);
+
+		ImGui::BeginChild("Folders");
+		if (ImGui::TreeNode(m_AssetDirectory.filename().string().c_str()))
 		{
-			m_CurrentDirectory = m_CurrentDirectory.parent_path();
+			m_CurrentDirectory = m_AssetDirectory;
+			DrawDirectoryNodes(m_AssetDirectory);
+			ImGui::TreePop();
 		}
+		ImGui::EndChild();
+
+		ImGui::TableSetColumnIndex(1);
+
+		ImGui::BeginChild("Files", { ImGui::GetContentRegionAvail().x, ImGui::GetWindowSize().y });
+		DrawDirectoryFiles(m_CurrentDirectory);
+		ImGui::EndChild();
+
+		ImGui::EndTable();
 	}
-
-	static float padding = 24.f;
-	static float thumbnailSize = 86.f;
-	static float cellsize = thumbnailSize + padding;
-
-	float panelWidth = ImGui::GetContentRegionAvail().x;
-
-	int columnCount = (int)(panelWidth / (cellsize));
-	columnCount = std::max(1, columnCount);
-	ImGui::Columns(columnCount, 0, false);
-
-	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.1f, 0.1f, 0.1f, 1.f));
-		
-	int i = 0;
-	for (auto& directory : std::filesystem::directory_iterator(m_CurrentDirectory))
-	{
-		ImGui::PushID(i++);
-
-		const auto& path = directory.path();
-		auto relativepath = std::filesystem::relative(path, m_AssetDirectory);
-
-		std::string filenameString = relativepath.filename().string();
-
-		if (directory.is_directory())
-		{
-			ImGui::Button(filenameString.c_str(), {thumbnailSize,thumbnailSize});
-
-			if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
-			{
-				m_CurrentDirectory /= path.filename();
-			}
-		}
-		else
-		{
-			ImGui::Button(filenameString.c_str(), { thumbnailSize,thumbnailSize });
-		}
-
-		//if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_SourceAllowNullID))
-		//{
-		//	auto relativePath = std::filesystem::relative(path, m_AssetDirectory);
-		//	const wchar_t* itemPath = relativePath.c_str();
-		//	ImGui::SetDragDropPayload("CONTENT_BROWSER_ITEM", itemPath, (wcslen(itemPath) + 1) * sizeof(wchar_t));
-		//	ImGui::EndDragDropSource();
-		//}
-
-		//ImGui::TextWrapped(filenameString.c_str());
-
-		ImGui::NextColumn();
-		ImGui::PopID();
-	}
-	ImGui::PopStyleColor();
-	ImGui::Columns(1);
 
 	ImGui::End();
 }
--- Source/Nexus/src/Nexus/Editor/ContentBrowser.h
+++ Source/Nexus/src/Nexus/Editor/ContentBrowser.h
@@ -11,6 +11,8 @@ namespace Nexus
 	private:
 		std::filesystem::path m_AssetDirectory, m_CurrentDirectory;
 
+		void DrawDirectoryNodes(std::filesystem::path path);
+		void DrawDirectoryFiles(std::filesystem::path path);
 	};
 }
 
