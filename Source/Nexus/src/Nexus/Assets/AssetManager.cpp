01ea540ce94fed9e40b3e6a166931f5d948ff2c3
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-05-16 06:24:33 +0530

    Added Default Asset

--- Source/Nexus/src/Nexus/Assets/AssetManager.cpp
+++ Source/Nexus/src/Nexus/Assets/AssetManager.cpp
@@ -1,11 +1,25 @@
 #include "nxpch.h"
 #include "AssetManager.h"
+#include "Renderer/Texture.h"
 
 Nexus::AssetManager* Nexus::AssetManager::s_Instance = nullptr;
 
 void Nexus::AssetManager::Initialize()
 {
 	s_Instance = new AssetManager();
+	
+	// Defaults
+	{
+		Ref<Asset> checker = Texture::Create("Resources/Textures/DefaultWhite.png");
+		checker->m_Id = 0;
+		
+		s_Instance->m_Assets[0] = checker;
+
+		Ref<Asset> sampler = Sampler::Create(SamplerFilter::Nearest, SamplerFilter::Linear,
+			SamplerWrapMode::Repeat, SamplerWrapMode::Repeat, SamplerWrapMode::Repeat);
+		sampler->m_Id = 1;
+		s_Instance->m_Assets[1] = sampler;
+	}
 }
 
 void Nexus::AssetManager::Shutdown()
