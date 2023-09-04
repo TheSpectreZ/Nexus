3119c9a9a47364b0f37db561ab5daddf5d3de406
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2023-09-04 21:31:34 +0530

    - Initial Network Engine Work

---
+++ Source/Engine/NxNetworkEngine/include/NxNetworkEngine/NetworkSocket.h
@@ -0,0 +1,68 @@
+#pragma once
+#include "NxCore/Base.h"
+
+#ifdef NEXUS_NETWORK_ENGINE_SHARED_BUILD
+#define NEXUS_NETWORK_ENGINE_API __declspec(dllexport)
+#else
+#define NEXUS_NETWORK_ENGINE_API __declspec(dllimport)
+#endif
+
+namespace Nexus
+{
+	// Currently Server Socket connnects to only one Client Socket
+
+	class NetworkSocket
+	{
+	public:
+		virtual ~NetworkSocket();
+	protected:
+		NetworkSocket(int type, int protocol);
+	protected:
+		struct INTERNAL;
+		INTERNAL* p_State = nullptr;
+	};
+
+	class ClientNetworkSocket_TCP : public NetworkSocket
+	{
+	public:
+		ClientNetworkSocket_TCP();
+		~ClientNetworkSocket_TCP() override = default;
+
+		bool Connect();
+
+		int Receive(char* buffer, size_t size);
+		int Send(const char* buffer, size_t size);
+	};
+	
+	class ServerNetworkSocket_TCP : public NetworkSocket
+	{
+	public:
+		ServerNetworkSocket_TCP();
+		~ServerNetworkSocket_TCP() override = default;
+
+		void Accept();
+
+		int Receive(char* buffer, size_t size);
+		int Send(const char* buffer, size_t size);
+	};
+	
+	class ClientNetworkSocket_UDP : public NetworkSocket
+	{
+	public:
+		ClientNetworkSocket_UDP();
+		~ClientNetworkSocket_UDP() override = default;
+
+		int Receive(char* buffer, size_t size);
+		int Send(const char* buffer, size_t size);
+	};
+	
+	class ServerNetworkSocket_UDP : public NetworkSocket
+	{
+	public:
+		ServerNetworkSocket_UDP();
+		~ServerNetworkSocket_UDP() override = default;
+
+		int Receive(char* buffer, size_t size);
+		int Send(const char* buffer, size_t size);
+	};
+}
---
+++ Source/Engine/NxNetworkEngine/src/NetworkSocket.cpp
@@ -0,0 +1,130 @@
+#include "NxNetworkEngine/NetworkSocket.h"
+#include "NxCore/Assertion.h"
+#include <WinSock2.h>
+
+#define NEXUS_NETWORK_PORT 54321
+#define NEXUS_HOST_NAME ""
+
+struct Nexus::NetworkSocket::INTERNAL
+{
+	// - socket handle
+	SOCKET socket;
+
+	// - socket address
+	sockaddr_in sockAddr;
+};
+
+Nexus::NetworkSocket::NetworkSocket(int type, int protocol)
+{
+	p_State = new INTERNAL;
+	p_State->socket = socket(AF_INET, type, protocol);
+
+	p_State->sockAddr.sin_family = AF_INET;
+	p_State->sockAddr.sin_port = htons(NEXUS_NETWORK_PORT);
+}
+
+Nexus::NetworkSocket::~NetworkSocket()
+{
+	closesocket(p_State->socket);
+	delete p_State;
+}
+
+Nexus::ClientNetworkSocket_TCP::ClientNetworkSocket_TCP()
+	:NetworkSocket(SOCK_STREAM,IPPROTO_TCP)
+{	
+	auto host = gethostbyname(NEXUS_HOST_NAME);
+	auto IP = inet_ntoa(*(in_addr*)*host->h_addr_list);
+
+	p_State->sockAddr.sin_addr.s_addr = inet_addr(IP);
+}
+
+bool Nexus::ClientNetworkSocket_TCP::Connect()
+{
+	int result = connect(p_State->socket, (sockaddr*)&p_State->sockAddr, sizeof(p_State->sockAddr));
+	return !(result == SOCKET_ERROR);
+}
+
+int Nexus::ClientNetworkSocket_TCP::Receive(char* buffer, size_t size)
+{
+	return recv(p_State->socket, buffer, size, 0);
+}
+
+int Nexus::ClientNetworkSocket_TCP::Send(const char* buffer, size_t size)
+{
+	return send(p_State->socket, buffer, size, 0);
+}
+
+Nexus::ServerNetworkSocket_TCP::ServerNetworkSocket_TCP()
+	:NetworkSocket(SOCK_STREAM, IPPROTO_TCP)
+{
+	p_State->sockAddr.sin_addr.s_addr = INADDR_ANY;
+
+	bind(p_State->socket, (sockaddr*)&p_State->sockAddr, sizeof(p_State->sockAddr));
+	listen(p_State->socket, SOMAXCONN);
+}
+
+void Nexus::ServerNetworkSocket_TCP::Accept()
+{
+	SOCKET sock{};
+	sockaddr_in addr{};
+	int addrSize = sizeof(addr);
+
+	sock = accept(p_State->socket, (sockaddr*)&addr, &addrSize);
+	NEXUS_ASSERT(sock == INVALID_SOCKET, "Socket Accept Failed");
+
+	closesocket(p_State->socket);
+	p_State->socket = sock;
+}
+
+int Nexus::ServerNetworkSocket_TCP::Receive(char* buffer, size_t size)
+{
+	return recv(p_State->socket, buffer, size, 0);
+}
+
+int Nexus::ServerNetworkSocket_TCP::Send(const char* buffer, size_t size)
+{
+	return send(p_State->socket, buffer, size, 0);
+}
+
+Nexus::ClientNetworkSocket_UDP::ClientNetworkSocket_UDP()
+	:NetworkSocket(SOCK_DGRAM, IPPROTO_UDP)
+{
+	auto host = gethostbyname(NEXUS_HOST_NAME);
+	auto IP = inet_ntoa(*(in_addr*)*host->h_addr_list);
+
+	p_State->sockAddr.sin_addr.s_addr = inet_addr(IP);
+}
+
+int Nexus::ClientNetworkSocket_UDP::Receive(char* buffer, size_t size)
+{
+	sockaddr_in addr{};
+	int addrSize = sizeof(addr);
+
+	return recvfrom(p_State->socket, buffer, size, 0, (sockaddr*)&addr, &addrSize);
+}
+
+int Nexus::ClientNetworkSocket_UDP::Send(const char* buffer, size_t size)
+{
+	return sendto(p_State->socket, buffer, size, 0, (sockaddr*)&p_State->sockAddr, sizeof(p_State->sockAddr));
+}
+
+Nexus::ServerNetworkSocket_UDP::ServerNetworkSocket_UDP()
+	:NetworkSocket(SOCK_DGRAM, IPPROTO_UDP)
+{
+	p_State->sockAddr.sin_addr.s_addr = INADDR_ANY;
+	bind(p_State->socket, (sockaddr*)&p_State->sockAddr, sizeof(p_State->sockAddr));
+}
+
+int Nexus::ServerNetworkSocket_UDP::Receive(char* buffer, size_t size)
+{
+	sockaddr addr{};
+	int addrSize = sizeof(addr);
+	
+	return recvfrom(p_State->socket, buffer, size, 0, (sockaddr*)&addr, &addrSize);
+}
+
+int Nexus::ServerNetworkSocket_UDP::Send(const char* buffer, size_t size)
+{
+	sockaddr_in addr{};
+	return sendto(p_State->socket, buffer, size, 0, (sockaddr*)&addr, sizeof(addr));
+}
